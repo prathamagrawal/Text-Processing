b7a1781c9a99f05a50d0335baddf7fecb5eacc1f
Pratham Agrawal <prathamagrawal1205@gmail.com>
2023-01-26 23:42:51 +0530

    Added flask server code

--- Readme.md
+++ Readme.md
@@ -6,3 +6,6 @@ blob file creation using js,
 file creation in python using dataframe
 temporary file storage 
 sending the blob using api
+
+
+# processin
--- flask-server/app.py
+++ flask-server/app.py
@@ -1,18 +1,44 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session,jsonify
 import pandas as pd
-import os
+from io import BytesIO
 from werkzeug.utils import secure_filename
+
+
     
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
  
 @app.route('/data')
 def data():
     return {"Flask Server":"Pratham"}
     # text="Upload any csv file"
     # return render_template('index.html',text1=text)
+    
+@app.route("/",methods=["POST"])
+def return_csv():
+    d=request.file
+    print(d)
+
+@app.route('/upload', methods=['POST'])
+def fileUpload():
+    file = request.files['file'] 
+    filename = file.filename
+    file_bytes = file.read()
+    file_content = BytesIO(file_bytes).readlines()
+    train=pd.DataFrame(file_content)
+    shape=train.shape[1]
+    train.columns=['col'+str(i) for i in range(shape)]
+    columns_to_exclude=list((train.select_dtypes(exclude=['object'])).columns)
+    
+    res=train[columns_to_exclude]
+    train=train.drop(columns_to_exclude,axis=1)
+    
 
 
 
+
+
+    response=""
+    return response
 
 if __name__=='__main__':
     app.run(debug = True)
